351a873e60a872b9710681ef55c20a1157deec72
iliabulavintsev <twintipper@mail.ru>
2017-04-18 17:53:54 +0300

    Add LoginActivity

---
+++ app/src/main/java/com/example/ilya/lorekeep/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.ilya.lorekeep;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ilya.lorekeep.topic.TopicActivity;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.util.VKUtil;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button login_button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login_button = (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VKSdk.login(LoginActivity.this, VKUtil.getCertificateFingerprint(getApplicationContext(), getApplicationContext().getPackageName()));
+                VKSdk.initialize(LoginActivity.this);
+            }
+        });
+
+        //auth vk
+
+        VKSdk.login(LoginActivity.this, VKUtil.getCertificateFingerprint(this, this.getPackageName()));
+        VKSdk.initialize(LoginActivity.this);
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // Пользователь успешно авторизовался
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+            @Override
+            public void onError(VKError error) {
+                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
